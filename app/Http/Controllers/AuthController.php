582cccde6547115d162e899892ed36cb5f76d85d
cybergaro <francyclimber@gmail.com>
2025-09-19 12:47:33 +0200

    fx

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
 use App\Services\BrevoMailer;
