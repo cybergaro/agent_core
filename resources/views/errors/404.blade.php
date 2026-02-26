6670d9b14aa58602627f6a282ba087db7cffe596
Francesco Garofolo <francyclimber@gmail.com>
2026-02-26 21:21:11 +0100

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title> Agent Core</title>
+
+    <link rel="shortcut icon" href="/img/logoBlu.png" type="image/x-icon">
+    
+    
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    
+    <script src="https://kit.fontawesome.com/3e5cf208fc.js" crossorigin="anonymous"></script>
+
+    @vite('resources/css/app.css')
+
+</head>
+
+<body class="overflow-hidden">
+    <div class="flex h-screen bg-gray-100 font-sans">
+
+        <main class="flex-1 overflow-y-auto">
+            <div class="h-full w-full flex items-center justify-center gap-5">
+                <img src="/img/logoBlu.png" alt="Logo" class="h-20 w-20">
+                <div>
+                    <h1 class="font-bold text-3xl">404</h1>
+                    <p>Risorsa non risponibile</p>
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
