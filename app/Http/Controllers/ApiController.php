5ff262ea114ef9ce0e5fb223e6e4b7b648d0e1cf
Francesco Garofolo <francyclimber@gmail.com>
2026-02-28 00:54:05 +0100

    impaginazione dati all'interno del google sheet

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -418,11 +418,29 @@ class ApiController extends Controller
 
             $range = 'Foglio1!A:D'; 
 
+            $categoryTranslations = [
+                'buy'          => 'Cerco un immobile da acquistare',
+                'sell'         => 'Vorrei vendere il mio immobile',
+                'evaluation'   => 'Richiesta valutazione gratuita',
+                'visit_buy'    => 'Visita per annuncio di vendita',
+                'rent'         => 'Cerco un immobile in affitto',
+                'let'          => 'Propongo il mio immobile per l\'affitto',
+                'visit_rent'   => 'Visita per annuncio di affitto',
+                'management'   => 'Gestione affitti / Property Management',
+                'mortgage'     => 'Informazioni su mutui e finanziamenti',
+                'technical'    => 'Informazioni tecniche o burocratiche',
+                'construction' => 'Cantieri e nuove costruzioni',
+                'job'          => 'Lavora con noi / Candidatura',
+                'other'        => 'Informazioni generali / Altra richiesta'
+            ];
+
             $values = [[
+                $message->created_at,
                 $request->name, 
-                $request->email,
                 $request->tel,
-                $request->text
+                $request->email,
+                $categoryTranslations[$request->category],
+                $request->message
             ]];
 
             $body = new \Google_Service_Sheets_ValueRange([
